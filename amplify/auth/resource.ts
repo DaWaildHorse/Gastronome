e9dcaca2908e60f1be64c8917e66e1ee0a723438
Andre <andeelou69@outlook.com>
2025-05-03 19:49:10 -0600

    wip : testing google auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from "@aws-amplify/backend";
+import { defineAuth, secret } from "@aws-amplify/backend";
 
 export const auth = defineAuth({
   loginWith: {
@@ -8,5 +8,22 @@ export const auth = defineAuth({
       verificationEmailBody: (createCode) =>
         `Use this code to confirm your account: ${createCode()}`,
     },
-  },
+    externalProviders :{
+      google: {
+        clientId: secret('869550831718-5rk9jufe6baqad8cit4lh7e4m909lkvf.apps.googleusercontent.com '),
+        clientSecret: secret('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'),
+               
+        attributeMapping: {
+          email: 'email'
+        }
+      },
+      callbackUrls: [
+        'http://localhost:3000/profile',
+        'https://https://main.d1nryih3xi6rcm.amplifyapp.com/profile'
+      ],
+      logoutUrls: ['http://localhost:3000/', 'https://main.d1nryih3xi6rcm.amplifyapp.com'],
+    }
+    
+  }
 });
+
