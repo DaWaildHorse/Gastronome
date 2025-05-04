7c012d6ab037b525160cd9066570281d28de2e95
Andre <andeelou69@outlook.com>
2025-05-03 19:26:16 -0600

    wip: google auth

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { Authenticator } from "@aws-amplify/ui-react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Authenticator>
+    <Authenticator socialProviders={['google']} >
       <App />
     </Authenticator>
   </React.StrictMode>
